0c87fc84b228e232fccd251b14f37eb739ecac43
NeneLawani <nene@lawani.com>
2023-05-25 08:07:13 -0700

    implemented the pint opcode

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *hold;
-	
+
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
